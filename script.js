55a7f4a0111467c01a81f0e4c8156d9e126bbb2d
saran sinha <saransinha1522@gmail.com>
2023-06-09 15:32:29 +0530

    some changes in responsive design and also added signup page

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function myFunc() {
-  const navbar = document.getElementById("navbar");
+  const navbar = document.getElementById("navbar")
   const btn = document.getElementById('iconBtn')
   const icon = document.getElementById('icon')
   if (navbar.className == "navbar")
   {
-    navbar.className += " responsive";
-    btn.classList.add('resicon')
+    navbar.className += " responsive"
+    btn.className += " res"
     btn.innerHTML=`<i class="fas fa-times"></i>`
   } 
     
   else{
      navbar.className = "navbar";
-     btn.classList.remove('resicon')
+     btn.className="icon"
      btn.innerHTML=`<i class="fa fa-bars"></i>`
   }
 }
